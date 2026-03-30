8dfbc4c8b1ab69f1d6541b98712f22ca23ba227a
gapuchi <arjun.adhia@gmail.com>
2026-03-30 00:31:47 -0600

    Update OpenClaw Version

--- flake.nix
+++ flake.nix
@@ -29,16 +29,16 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          nodejs = pkgs.nodejs_22;
+          nodejs = pkgs.nodejs_24;
 
-          version = "2026.2.6-3";
+          version = "2026.3.28";
 
           # Combine tarball + lockfile into a proper source
           openclawSrc = pkgs.stdenv.mkDerivation {
             name = "openclaw-src-${version}";
             src = pkgs.fetchurl {
               url = "https://registry.npmjs.org/openclaw/-/openclaw-${version}.tgz";
-              hash = "sha256-zDMRFzjdetdw0Q47uqCIKHoqV7UwjxKnS6L9u2VoTJM=";
+              hash = "sha256-/XCaOfXRTAL3c/N+GKq8D1GAw3N7Jvce+pCwba/D7cI=";
             };
             phases = [
               "unpackPhase"
@@ -58,7 +58,7 @@
             src = openclawSrc;
 
             # Generated with: prefetch-npm-deps package-lock.json
-            npmDepsHash = "sha256-NPUq7InJJI00fVhmN6VUVcR7+lZrgl6AFNPdRYGb/Ms=";
+            npmDepsHash = "sha256-v2UB5lpEJDHneLn7Uz5utsbn85CxJOTdwJkphXjJbBY=";
 
             nodejs = nodejs;
 
